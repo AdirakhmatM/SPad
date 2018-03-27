4c11003b11dacb7ac1742e32755d4009e789c827
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2018-03-27 09:41:34 +0800

    Membuat Class New

---
+++ soundspeaker/src/Praktek1ad/Praktek1ad.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktek1ad;
+
+/**
+ *
+ * @author Adrama
+ */
+public class Praktek1ad {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
