0ce27c3c806291c210462b616558934f8db5cd03
AdirakhmatM <32364278+AdirakhmatM@users.noreply.github.com>
2018-03-27 13:50:00 +0800

    Create Attribut dan Behavior

--- soundspeaker/src/Praktek1ad/Soundspeaker.java
+++ soundspeaker/src/Praktek1ad/Soundspeaker.java
@@ -10,5 +10,17 @@ package Praktek1ad;
  * @author Adrama
  */
 public class Soundspeaker {
+    String merk;
+    String harga;
+    String type;
+    String jenis;
+    String kualitas;
     
+    void listensound(){
+    system.out.println("Merk :"+merk);
+    system.out.println("Harga :"+harga);
+    system.out.println("Type :"+type);
+    system.out.println("Jenis :"+jenis);
+    system.out.println("Kualitas :"+kualitas);
+    }
 }
